d9712a299bab9dec0c156d8278a1a4ef85cdacd4
AlekLima <167828616+AlekLima@users.noreply.github.com>
2024-06-10 15:17:35 -0300

    Casos de Uso e Design Patterns - Repository Pattern

---
+++ src/repositories/prisma-pets-repository.ts
@@ -0,0 +1,13 @@
+import { prisma }  from '@/lib/prisma'
+import { Prisma } from '@prisma/client'
+
+
+export class PrismaPetsRepository {
+    async create(data: Prisma.PetCreateInput) {
+        const pet = await prisma.pet.create({
+            data,
+        })
+
+        return pet
+    }
+}
