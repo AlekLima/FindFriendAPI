49b486c62439a38068a8c34c9fe1b6f944f9fa9e
AlekLima <167828616+AlekLima@users.noreply.github.com>
2024-06-28 06:42:19 -0300

    Controllers & Testes E2E - Repositório de academias

--- src/repositories/prisma/prisma-orgs-repository.ts
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -1,11 +1,16 @@
 import { prisma } from "@/lib/prisma";
 import { Prisma, Org } from "@prisma/client"
-import { OrgsRepository } from '../orgs-repository'
+import { OrgsRepository, findManyNearbyParams } from '../orgs-repository'
 
 export class PrismaOrgsRepository implements OrgsRepository {
-    async findById(id: string): Promise<Org | null> {
-        throw new Error('Method not implemented.')
+    async findById(id: string)  {
+        const org = await prisma.org.findUnique({
+            where: {
+                id,
+            }
+        })
 
+        return org
     }
 
     async findByEmail(email: string) {
@@ -18,8 +23,33 @@ export class PrismaOrgsRepository implements OrgsRepository {
         return org
     }
 
+    async findManyNearby({ latitude, longitude }: findManyNearbyParams) {
+        const orgs = await prisma.$queryRaw<Org[]>`
+        SELECT * from orgs
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+        `
+
+        return orgs
+    }
+
+    async searchMany(query: string, page: number) {
+        const orgs =await prisma.org.findMany({
+            where: {
+                email: {
+                    contains: query,
+                },
+            },
+            take: 20,
+            skip: (page - 1) * 20
+        })
+
+        return orgs
+    }
+    
+    
+
     async create(data: Prisma.OrgCreateInput) {
-        const org = await prisma.org.create({S
+        const org = await prisma.org.create({
             data,
         })
 
