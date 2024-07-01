08d38625b5f6e6f7f43a6fc26271f08f941817f0
AlekLima <167828616+AlekLima@users.noreply.github.com>
2024-07-01 06:10:01 -0300

    Controllers & Testes E2E - Teste E2E da autenticação

--- src/http/controllers/authenticateOrg.spec.ts
+++ src/http/controllers/authenticateOrg.spec.ts
@@ -1,5 +1,37 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 
-test('ok', () => {
+describe('Authenticate (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
 
-}) 
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authenticate', async () => {
+        await request(app.server).post('/orgs')
+        .send({
+            email: 'johndoeeee@example.com',
+            city: 'Fortaleza',
+            password: '1234567',
+            description: 'blablalba',
+            phone: '8599643848',
+            latitude:  -3.702784,
+            longitude: -38.6433024
+        })
+
+        const response = await request(app.server).post('/session')
+        .send({
+            email: 'johndoeeee@example.com',
+            password: '1234567'
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String),
+        })
+    })
+})
--- src/http/controllers/registerPets.spec.ts
+++ src/http/controllers/registerPets.spec.ts
@@ -1,5 +1,25 @@
-import { test } from 'vitest'
+import  request  from 'supertest'
+import { describe, it } from 'vitest'
+import { afterAll,beforeAll, expect } from 'vitest'
+import { app } from '@/app'
 
-test('ok', () => {
+describe('Register Pet (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+    
+    it('should be able to register Pet', async() => {
+        const response = await request(app.server)
+        .post('/pets')
+        .send({
+            name:  'Roberto',
+            characteristics: 'Legalzão',
+            description: 'Pastor Alemão',
+        })
 
-}) 
+        expect(response.statusCode).toEqual(201)
+    })
+})
