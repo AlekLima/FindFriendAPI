3c2fede243590f4b948390b9b4f74431f3c11f9a
AlekLima <167828616+AlekLima@users.noreply.github.com>
2024-07-15 12:19:38 -0300

    Controllers & Testes E2E - Outros controllers da academia

---
+++ src/http/controllers/orgs/nearby.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFetchNearbyOrgsUseCase } from '@/use-cases/factories/make-fetch-nearby-orgs-use-case'
+
+export async function nearby (request: FastifyRequest, reply: FastifyReply) {
+    const nearbyOrgsQuerySchema = z.object({
+        latitude: z.number().refine((value) => {
+            return Math.abs(value) <= 90
+        }),
+        longitude: z.number().refine((value) => {
+            return Math.abs(value) <= 180
+        }),
+    })
+
+    const { latitude, longitude } = 
+    nearbyOrgsQuerySchema.parse(request.body)
+
+    const fetchNearbyOrgsUseCase = makeFetchNearbyOrgsUseCase()
+
+    const { orgs } = await fetchNearbyOrgsUseCase.execute({
+        orgLatitude: latitude,
+        orgLongitude: longitude,
+    })
+
+    return reply.status(201).send({
+        orgs,
+    })
+}
---
+++ src/http/controllers/orgs/search.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeSearchOrgUseCase } from '@/use-cases/factories/make-search-orgs-use-case'
+
+export async function search (request: FastifyRequest, reply: FastifyReply) {
+    const searchOrgsQuerySchema = z.object({
+        q: z.string(),
+        page: z.coerce.number().min(1).default(1),
+    })
+
+    const { q, page } = 
+    searchOrgsQuerySchema.parse(request.query)
+
+    const searchOrgsUseCase = makeSearchOrgUseCase()
+
+    const { orgs } = await searchOrgsUseCase.execute({
+        query: q,
+        page
+    })
+
+    return reply.status(201).send({
+        orgs,
+    })
+}
